15ff77406708592ed2b75b29d2f57857c897766d
poojithakinavicmj763 <poojitha.k_intcse23@avit.ac.in>
2025-12-25 17:53:43 +0530

    corrected Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,21 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter age: ");
+        System.out.print("Enter age:");
         int age = scanner.nextInt();
-        System.out.print("Enter bill amount: ");
+        System.out.print("Enter Bill amount:");
         double billAmount = scanner.nextDouble();
         double discount = 0.0;
-        double finalBill;
         if (age >= 60) {
             System.out.println("Senior Citizen");
-            discount = billAmount * 0.30;
-            if (discount > 300) {
-                discount = 300;
-            }
+            discount = Math.min(billAmount * 0.30, 300);
         } else {
             System.out.println("Normal User");
         }
-        finalBill = billAmount - discount;
-        System.out.println("Final Bill Amount:");
-        System.out.printf("%.2f%n", finalBill);
+        double finalBillAmount = billAmount - discount;
+        System.out.println("Final Bill amount: " + String.format("%.2f", finalBillAmount));
         scanner.close();
     }
 }
