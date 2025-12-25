554c923c20b5d2940b651933fe757ba1dc00a786
poojithakinavicmj763 <poojitha.k_intcse23@avit.ac.in>
2025-12-25 11:36:00 +0530

    Updated Main.java as per HCL assignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,25 @@
 import java.util.Scanner;
-class FoodBillDiscount {
+public class Main {
     public static void main(String[] args) {
-
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter age: ");
-            int age = scanner.nextInt();
-
-            System.out.print("Enter Bill amount: ");
-            double billAmount = scanner.nextDouble();
-
-            double finalBill = billAmount;
-
-            if (age >= 60) {
-                System.out.println("Senior Citizen");
-
-                double discount = billAmount * 0.30;
-
-                // Maximum discount cap of 300
-                if (discount > 300) {
-                    discount = 300;
-                }
-
-                finalBill = billAmount - discount;
-            } else {
-                System.out.println("Normal User");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age = scanner.nextInt();
+        System.out.print("Enter bill amount: ");
+        double billAmount = scanner.nextDouble();
+        double discount = 0.0;
+        double finalBill;
+        if (age >= 60) {
+            System.out.println("Senior Citizen");
+            discount = billAmount * 0.30;
+            if (discount > 300) {
+                discount = 300;
             }
-
-            System.out.println("Final Bill amount:");
-            System.out.printf("%.2f", finalBill);
+        } else {
+            System.out.println("Normal User");
         }
+        finalBill = billAmount - discount;
+        System.out.println("Final Bill Amount:");
+        System.out.printf("%.2f%n", finalBill);
+        scanner.close();
     }
 }
